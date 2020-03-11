5dc45b0ca4c048a32e1f162f08a8df49a7219df6
David van Oudheusden <d.vanoudheusden@rocva.nl>
2020-03-11 16:06:39 +0100

    alles bij elkaar

---
+++ les 2/strings1.py
@@ -0,0 +1,29 @@
+# Strings
+
+### opdracht - print Hello World ###
+
+# 1. 'Vul' een variabele met de string 'Hello World!'
+# 2. print deze op het scherm
+
+hello = 'Hello World'
+print(hello)
+
+
+### opdracht - print Mijn naam is.... ###
+
+# 1. 'Vul' een variabele met de input van de gebruiker 
+# 2. print deze op het scherm
+
+
+input_gebruiker = raw_input('')
+
+
+### opdracht - strings aan elkaar ###
+
+#  'Hottentottententententoonstelling ' is een woord. Maar hij is uit elkaar. 
+# 1. Maak het aan elkaar met door twee strings te concatenaten
+# 2. print deze op het scherm
+# 3. Zet er ook nog het woordje terrein op dezelfde wijze erachter. Print het op het scherm
+
+string1 = 'Hottentottenten'
+string2 = 'tententoonstelling'
